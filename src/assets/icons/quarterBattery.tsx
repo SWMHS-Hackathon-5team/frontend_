798aa9c865aa007f0ad15a0cbdef2550e8c0f60e
Ethen1264 <jxorhks86909763@gmail.com>
2024-10-29 21:17:36 +0900

    add icon setting

---
+++ src/assets/icons/quarterBattery.tsx
@@ -0,0 +1,29 @@
+const QuarterBattery = () => {
+  return (
+    <svg
+      width='85'
+      height='85'
+      viewBox='0 0 85 85'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <rect width='85' height='85' rx='42.5' fill='#282A2E' />
+      <path
+        d='M43 27H34C33 30.8333 30.5 43 30.5 43C36 43 39.5 47 39.5 53H51.5M51.5 53C51.5 55.4853 53.5147 57.5 56 57.5C58.4853 57.5 60.5 55.4853 60.5 53C60.5 50.5147 58.4853 48.5 56 48.5C53.5147 48.5 51.5 50.5147 51.5 53ZM34 53C34 55.4853 31.9853 57.5 29.5 57.5C27.0147 57.5 25 55.4853 25 53C25 50.5147 27.0147 48.5 29.5 48.5C31.9853 48.5 34 50.5147 34 53Z'
+        stroke='white'
+        stroke-width='3'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+      />
+      <circle cx='42.5' cy='42.5' r='33.5' stroke='#45474D' stroke-width='4' />
+      <path
+        d='M76 42.5C76 23.9985 61.2254 9 43 9'
+        stroke='#66F1B5'
+        stroke-width='4'
+        stroke-linecap='round'
+      />
+    </svg>
+  )
+}
+
+export default QuarterBattery
---
+++ src/assets/icons/smallHelp.tsx
@@ -0,0 +1,95 @@
+const index = () => {
+  return (
+    <svg
+      width='99'
+      height='111'
+      viewBox='0 0 99 111'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <g filter='url(#filter0_d_37_263)'>
+        <mask
+          id='path-1-outside-1_37_263'
+          maskUnits='userSpaceOnUse'
+          x='10'
+          y='8'
+          width='79'
+          height='91'
+          fill='black'
+        >
+          <rect fill='white' x='10' y='8' width='79' height='91' />
+          <path
+            fill-rule='evenodd'
+            clip-rule='evenodd'
+            d='M37 10C23.1929 10 12 21.1929 12 35V60C12 73.8071 23.1929 85 37 85H42.5L49.4282 94.75L56.3564 85H62C75.8071 85 87 73.8071 87 60V35C87 21.1929 75.8071 10 62 10H37Z'
+          />
+        </mask>
+        <path
+          fill-rule='evenodd'
+          clip-rule='evenodd'
+          d='M37 10C23.1929 10 12 21.1929 12 35V60C12 73.8071 23.1929 85 37 85H42.5L49.4282 94.75L56.3564 85H62C75.8071 85 87 73.8071 87 60V35C87 21.1929 75.8071 10 62 10H37Z'
+          fill='url(#paint0_linear_37_263)'
+        />
+        <path
+          d='M42.5 85L44.1304 83.8415L43.5324 83H42.5V85ZM49.4282 94.75L47.7979 95.9085L49.4282 98.2028L51.0586 95.9085L49.4282 94.75ZM56.3564 85V83H55.3241L54.7261 83.8415L56.3564 85ZM14 35C14 22.2974 24.2975 12 37 12V8C22.0883 8 10 20.0883 10 35H14ZM14 60V35H10V60H14ZM37 83C24.2974 83 14 72.7025 14 60H10C10 74.9117 22.0883 87 37 87V83ZM42.5 83H37V87H42.5V83ZM51.0586 93.5915L44.1304 83.8415L40.8697 86.1585L47.7979 95.9085L51.0586 93.5915ZM54.7261 83.8415L47.7979 93.5915L51.0586 95.9085L57.9868 86.1585L54.7261 83.8415ZM62 83H56.3564V87H62V83ZM85 60C85 72.7025 74.7025 83 62 83V87C76.9117 87 89 74.9117 89 60H85ZM85 35V60H89V35H85ZM62 12C74.7025 12 85 22.2975 85 35H89C89 20.0883 76.9117 8 62 8V12ZM37 12H62V8H37V12Z'
+          fill='white'
+          mask='url(#path-1-outside-1_37_263)'
+        />
+        <path
+          d='M43.95 29V43.55H57.95V29H65.3V66H57.95V50H43.95V66H36.55V29H43.95Z'
+          fill='white'
+        />
+      </g>
+      <defs>
+        <filter
+          id='filter0_d_37_263'
+          x='0'
+          y='0'
+          width='99'
+          height='110.203'
+          filterUnits='userSpaceOnUse'
+          color-interpolation-filters='sRGB'
+        >
+          <feFlood flood-opacity='0' result='BackgroundImageFix' />
+          <feColorMatrix
+            in='SourceAlpha'
+            type='matrix'
+            values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
+            result='hardAlpha'
+          />
+          <feOffset dy='2' />
+          <feGaussianBlur stdDeviation='5' />
+          <feComposite in2='hardAlpha' operator='out' />
+          <feColorMatrix
+            type='matrix'
+            values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0'
+          />
+          <feBlend
+            mode='normal'
+            in2='BackgroundImageFix'
+            result='effect1_dropShadow_37_263'
+          />
+          <feBlend
+            mode='normal'
+            in='SourceGraphic'
+            in2='effect1_dropShadow_37_263'
+            result='shape'
+          />
+        </filter>
+        <linearGradient
+          id='paint0_linear_37_263'
+          x1='12'
+          y1='52.375'
+          x2='87'
+          y2='52.375'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stop-color='#3689FF' />
+          <stop offset='1' stop-color='#B20CFF' />
+        </linearGradient>
+      </defs>
+    </svg>
+  )
+}
+
+export default index
