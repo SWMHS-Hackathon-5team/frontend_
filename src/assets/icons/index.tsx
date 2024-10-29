6cb7397d76117ab6d9d30353b6370bf183e31452
dutexion <dutexion@gmail.com>
2024-10-30 04:45:07 +0900

    fix error

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -1,6 +1,3 @@
 export { default as Help } from './help'
 export { default as SmallHelp } from './smallHelp'
-export { default as HalfBattery } from './halfBattery'
-export { default as QuarterBattery } from './quarterBattery'
-export { default as ThreeQuarterBattery } from './threeQuarterBattery'
-export { default as FullBattery } from './fullBattery'
+export { default as Battery } from './Battery'
